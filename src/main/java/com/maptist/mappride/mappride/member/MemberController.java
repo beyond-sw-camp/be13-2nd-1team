c684399f6088f4921534fb20189de54bdf2100ca
ChaGyoungtae <113877917+ChaGyoungtae@users.noreply.github.com>
2025-02-17 20:41:28 +0900

    Revert "1. Amazon S3 bucket을 이용한 이미지 업로드, 삭제, 2. 위도, 경도 입력받으면 도로명주소로 변환해주는 기능 구현"

--- src/main/java/com/maptist/mappride/mappride/member/MemberController.java
+++ src/main/java/com/maptist/mappride/mappride/member/MemberController.java
@@ -1,15 +1,11 @@
 package com.maptist.mappride.mappride.member;
 
-import com.maptist.mappride.mappride.config.jwt.DTO.SecurityUserDto;
 import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Slf4j
 public class MemberController {
 
     @GetMapping("/test")
